f2cbbf715f191667873b4944937ac59a51b15969
devhuy1011 <devhuy1011@gmail.com>
2023-11-05 23:36:31 +0700

    devhuy - update

---
+++ src/views/SignIn/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+import useWindowSize, { Size } from 'hooks/useWindowSize'
+
+import Menu from 'components/Menu'
+
+/** Style */
+import * as S from './style'
+
+interface Props {
+  apr?: any
+}
+
+const Global: React.FC<Props> = () => {
+  const history = useHistory()
+  const size: Size = useWindowSize()
+  const isMobile = size.width && size.width < 600
+
+  const openTab = (link) => {
+    window.open(link)
+  }
+
+  return (
+    <S.Wrapper>
+      <Menu />
+      <S.Header>
+        <h1>Hello World!!</h1>
+      </S.Header>
+    </S.Wrapper>
+  )
+}
+
+export default React.memo(Global, (prevProps, nextProps) => prevProps.apr === nextProps.apr)
---
+++ src/views/SignIn/style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import color from 'style/color'
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  button {
+    border: none;
+  }
+`
+const Header = styled.div`
+  width: 100%;
+  height: 100%;
+  button {
+    border: none;
+  }
+`
+
+export { Wrapper, Header }

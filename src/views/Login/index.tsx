d9c86ca044743c4753fff26994f4024911aa1204
JoyceA <anhtranviet1120@gmail.com>
2023-11-07 00:19:48 +0700

    eng update

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 
 import useWindowSize, { Size } from 'hooks/useWindowSize'
+import LogoB from 'assets/logo/big_logo.png'
+import Foxy from 'assets/signup/metamask.svg'
+import Google from 'assets/signup/google-oauth2.svg'
 
 import Menu from 'components/Menu'
+import { Image, Button, Input, Checkbox } from 'antd'
 
 /** Style */
+import { CheckboxChangeEvent } from 'antd/es/checkbox'
 import * as S from './style'
 
 interface Props {
@@ -14,19 +19,80 @@ interface Props {
 
 const Login: React.FC<Props> = () => {
   const history = useHistory()
+  const [isCheck, setIsCheck] = useState(false)
   const size: Size = useWindowSize()
   const isMobile = size.width && size.width < 600
 
   const openTab = (link) => {
     window.open(link)
   }
+  const onChange = (e: CheckboxChangeEvent) => {
+    // console.log(`check = ${e.target.checked}`)
+    setIsCheck(e.target.checked)
+  }
 
   return (
     <S.Wrapper>
-      <Menu />
-      <S.Header>
-        <h1>Lo gin!!</h1>
-      </S.Header>
+      <S.Title>
+        <S.Logo>
+          <Image src={LogoB} width={150} preview={false} />
+        </S.Logo>
+        <hr />
+        <h1> Đăng Nhập</h1>
+        <hr />
+        <S.Button>
+          <S.Metamask>
+            <Image src={Foxy} width={20} height={18} preview={false} />
+            Metamask
+          </S.Metamask>
+          <S.Google>
+            <Image src={Google} width={20} height={18} preview={false} />
+            Google
+          </S.Google>
+          <S.Plus>
+            <p> + </p>
+          </S.Plus>
+        </S.Button>
+        <hr />
+        <S.Span>
+          <span> Hoặc </span>
+        </S.Span>
+        <hr />
+        <S.Form>
+          <S.Account>
+            <S.Description>Tên người dùng hay email</S.Description>
+            <Input placeholder="Nhập ccm email vào đây" />
+          </S.Account>
+          <hr />
+          <S.Password>
+            <S.Description>Mật khẩu</S.Description>
+            <Input placeholder="Nhập nốt cái pát guột vào đây" />
+          </S.Password>
+          <hr />
+          <S.Check>
+            <Checkbox defaultChecked={false} onChange={onChange}>
+              Tôi sử dụng Google Authenticator
+            </Checkbox>
+          </S.Check>
+          <hr />
+          {isCheck ? (
+            <S.Auth>
+              <hr />
+              <S.Description>Mã Google Authenticator</S.Description>
+              <Input placeholder="Còn cái mã nữa" />
+              <hr />
+            </S.Auth>
+          ) : null}
+          <Button disabled type="primary" danger>
+            <S.Login>
+              Đăng nhập
+            </S.Login>
+          </Button>
+        </S.Form>
+      </S.Title>
+      <S.Background>
+        <h1>Abc xyZ</h1>
+      </S.Background>
     </S.Wrapper>
   )
 }
--- src/views/Login/style.ts
+++ src/views/Login/style.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 import color from 'style/color'
+import background from 'assets/signup/bg-login.png'
 
-const Wrapper = styled.div`
+export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
@@ -9,12 +10,153 @@ const Wrapper = styled.div`
     border: none;
   }
 `
-const Header = styled.div`
-  width: 100%;
+export const Title = styled.div`
+  min-width: 380px;
+  padding: 20px;
   height: 100%;
   button {
     border: none;
   }
+  > hr {
+    border: none;
+  }
 `
-
-export { Wrapper, Header }
+export const Form = styled.div`
+  width: 100%;
+  height: auto;
+  > hr {
+    border: none;
+  }
+  > button {
+    width: 100%;
+    height: 38px;
+  }
+  .ant-btn-primary.ant-btn-dangerous:disabled {
+    color: #fff;
+    background-color: rgb(214, 55, 65);
+    opacity: .35;
+  }
+`
+export const Account = styled.div`
+  width: 100%;
+  height: 72px;
+  > input {
+    width: 100%;
+    height: 40px;
+    padding: 0px 16px;
+    font-size: 15px;
+    background-color: #f2f2f2;
+  }
+`
+export const Password = styled.div`
+  width: 100%;
+  height: 72px;
+  > input {
+    width: 100%;
+    height: 40px;
+    padding: 0px 16px;
+    font-size: 15px;
+    background-color: #f2f2f2;
+  }
+`
+export const Description = styled.div`
+  width: 100%;
+  height: 32px;
+  padding: 0px 0px 8px;
+  color: rgb(133, 133, 133);
+`
+export const Check = styled.div`
+  width: 100%;
+  height: 24px;
+  gap: 5px;
+  .ant-checkbox-wrapper {
+    .ant-checkbox-checked .ant-checkbox-inner {
+      background-color: red;
+    }
+    &:hover {
+      .ant-checkbox-checked .ant-checkbox-inner {
+        background-color: red;
+      }
+    }
+  }
+`
+export const Auth = styled.div`
+  width: 100%;
+  > hr {
+    border: none;
+  }
+  > input {
+    width: 100%;
+    height: 40px;
+    padding: 0px 16px;
+    font-size: 15px;
+    background-color: #f2f2f2;
+  }
+`
+export const Login = styled.div`
+  width: 100%;
+  height: 40px;
+`
+export const Button = styled.div`
+  width: 340px;
+  height: 40px;
+  display: flex;
+  gap: 3px;
+  button {
+    border: none;
+  }
+`
+export const Metamask = styled.div`
+  width: 150px;
+  height: 40px;
+  padding: 5px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #e5e7eb;
+  gap: 3px;
+  font-size: 15px;
+`
+export const Google = styled.div`
+  width: 150px;
+  height: 40px;
+  padding: 5px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #e5e7eb;
+  gap: 3px;
+  font-size: 15px;
+`
+export const Plus = styled.div`
+  width: 44px;
+  height: 40px;
+  padding: 5px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #e5e7eb;
+  gap: 3px;
+  font-size: 15px;
+  background-color: #f2f2f2;
+`
+export const Span = styled.div`
+  width: 100%;
+  text-align: center;
+  justify-items: center;
+`
+export const Background = styled.div`
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  background-image: url(${background});
+  background-size: cover;
+  background-repeat: no-repeat;
+`
+export const Logo = styled.div`
+  max-width: auto;
+  height: auto;
+`
